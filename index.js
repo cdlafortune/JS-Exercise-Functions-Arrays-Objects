65c8238847fef92f27c4e15a718be7cc91c403ce
cdsuddreth <cdsuddreth@gmail.com>
2020-02-13 19:30:07 -0500

    Finished all required and stretch exercises

--- index.js
+++ index.js
@@ -98,58 +98,58 @@ function makeSmartPerson(smartName) {
 
 // ⭐️ Example Test Data ⭐️
 
-var inventory = [
-  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
-  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
-  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
-  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
-  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
-  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
-  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
-  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
-  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 },
-  { id: 15, car_make: "Dodge", car_model: "Intrepid", car_year: 2000 },
-  { id: 16, car_make: "Mitsubishi", car_model: "Montero Sport", car_year: 2001 },
-  { id: 17, car_make: "Buick", car_model: "Skylark", car_year: 1987 },
-  { id: 18, car_make: "Geo", car_model: "Prizm", car_year: 1995 },
-  { id: 19, car_make: "Oldsmobile", car_model: "Bravada", car_year: 1994 },
-  { id: 20, car_make: "Mazda", car_model: "Familia", car_year: 1985 },
-  { id: 21, car_make: "Chevrolet", car_model: "Express 1500", car_year: 2003 },
-  { id: 22, car_make: "Jeep", car_model: "Wrangler", car_year: 1997 },
-  { id: 23, car_make: "Eagle", car_model: "Talon", car_year: 1992 },
-  { id: 24, car_make: "Toyota", car_model: "MR2", car_year: 2003 },
-  { id: 25, car_make: "BMW", car_model: "525", car_year: 2005 },
-  { id: 26, car_make: "Cadillac", car_model: "Escalade", car_year: 2005 },
-  { id: 27, car_make: "Infiniti", car_model: "Q", car_year: 2000 },
-  { id: 28, car_make: "Suzuki", car_model: "Aerio", car_year: 2005 },
-  { id: 29, car_make: "Mercury", car_model: "Topaz", car_year: 1993 },
-  { id: 30, car_make: "BMW", car_model: "6 Series", car_year: 2010 },
-  { id: 31, car_make: "Pontiac", car_model: "GTO", car_year: 1964 },
-  { id: 32, car_make: "Dodge", car_model: "Ram Van 3500", car_year: 1999 },
-  { id: 33, car_make: "Jeep", car_model: "Wrangler", car_year: 2011 },
-  { id: 34, car_make: "Ford", car_model: "Escort", car_year: 1991 },
-  { id: 35, car_make: "Chrysler", car_model: "300M", car_year: 2000 },
-  { id: 36, car_make: "Volvo", car_model: "XC70", car_year: 2003 },
-  { id: 37, car_make: "Oldsmobile", car_model: "LSS", car_year: 1997 },
-  { id: 38, car_make: "Toyota", car_model: "Camry", car_year: 1992 },
-  { id: 39, car_make: "Ford", car_model: "Econoline E250", car_year: 1998 },
-  { id: 40, car_make: "Lotus", car_model: "Evora", car_year: 2012 },
-  { id: 41, car_make: "Ford", car_model: "Mustang", car_year: 1965 },
-  { id: 42, car_make: "GMC", car_model: "Yukon", car_year: 1996 },
-  { id: 43, car_make: "Mercedes-Benz", car_model: "R-Class", car_year: 2009 },
-  { id: 44, car_make: "Audi", car_model: "Q7", car_year: 2012 },
-  { id: 45, car_make: "Audi", car_model: "TT", car_year: 2008 },
-  { id: 46, car_make: "Oldsmobile", car_model: "Ciera", car_year: 1995 },
-  { id: 47, car_make: "Volkswagen", car_model: "Jetta", car_year: 2007 },
-  { id: 48, car_make: "Dodge", car_model: "Magnum", car_year: 2008 },
-  { id: 49, car_make: "Chrysler", car_model: "Sebring", car_year: 1996 },
-  { id: 50, car_make: "Lincoln", car_model: "Town Car", car_year: 1999 }
-]
+// var inventory = [
+//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+//   { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+//   { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+//   { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+//   { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+//   { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+//   { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+//   { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+//   { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+//   { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+//   { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+//   { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+//   { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 },
+//   { id: 15, car_make: "Dodge", car_model: "Intrepid", car_year: 2000 },
+//   { id: 16, car_make: "Mitsubishi", car_model: "Montero Sport", car_year: 2001 },
+//   { id: 17, car_make: "Buick", car_model: "Skylark", car_year: 1987 },
+//   { id: 18, car_make: "Geo", car_model: "Prizm", car_year: 1995 },
+//   { id: 19, car_make: "Oldsmobile", car_model: "Bravada", car_year: 1994 },
+//   { id: 20, car_make: "Mazda", car_model: "Familia", car_year: 1985 },
+//   { id: 21, car_make: "Chevrolet", car_model: "Express 1500", car_year: 2003 },
+//   { id: 22, car_make: "Jeep", car_model: "Wrangler", car_year: 1997 },
+//   { id: 23, car_make: "Eagle", car_model: "Talon", car_year: 1992 },
+//   { id: 24, car_make: "Toyota", car_model: "MR2", car_year: 2003 },
+//   { id: 25, car_make: "BMW", car_model: "525", car_year: 2005 },
+//   { id: 26, car_make: "Cadillac", car_model: "Escalade", car_year: 2005 },
+//   { id: 27, car_make: "Infiniti", car_model: "Q", car_year: 2000 },
+//   { id: 28, car_make: "Suzuki", car_model: "Aerio", car_year: 2005 },
+//   { id: 29, car_make: "Mercury", car_model: "Topaz", car_year: 1993 },
+//   { id: 30, car_make: "BMW", car_model: "6 Series", car_year: 2010 },
+//   { id: 31, car_make: "Pontiac", car_model: "GTO", car_year: 1964 },
+//   { id: 32, car_make: "Dodge", car_model: "Ram Van 3500", car_year: 1999 },
+//   { id: 33, car_make: "Jeep", car_model: "Wrangler", car_year: 2011 },
+//   { id: 34, car_make: "Ford", car_model: "Escort", car_year: 1991 },
+//   { id: 35, car_make: "Chrysler", car_model: "300M", car_year: 2000 },
+//   { id: 36, car_make: "Volvo", car_model: "XC70", car_year: 2003 },
+//   { id: 37, car_make: "Oldsmobile", car_model: "LSS", car_year: 1997 },
+//   { id: 38, car_make: "Toyota", car_model: "Camry", car_year: 1992 },
+//   { id: 39, car_make: "Ford", car_model: "Econoline E250", car_year: 1998 },
+//   { id: 40, car_make: "Lotus", car_model: "Evora", car_year: 2012 },
+//   { id: 41, car_make: "Ford", car_model: "Mustang", car_year: 1965 },
+//   { id: 42, car_make: "GMC", car_model: "Yukon", car_year: 1996 },
+//   { id: 43, car_make: "Mercedes-Benz", car_model: "R-Class", car_year: 2009 },
+//   { id: 44, car_make: "Audi", car_model: "Q7", car_year: 2012 },
+//   { id: 45, car_make: "Audi", car_model: "TT", car_year: 2008 },
+//   { id: 46, car_make: "Oldsmobile", car_model: "Ciera", car_year: 1995 },
+//   { id: 47, car_make: "Volkswagen", car_model: "Jetta", car_year: 2007 },
+//   { id: 48, car_make: "Dodge", car_model: "Magnum", car_year: 2008 },
+//   { id: 49, car_make: "Chrysler", car_model: "Sebring", car_year: 1996 },
+//   { id: 50, car_make: "Lincoln", car_model: "Town Car", car_year: 1999 }
+// ]
 
 
 /**
@@ -236,15 +236,9 @@ function getCarInfoById(carInventory, carId) {
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
 function sortCarInventory(inv) {
-  let arr = [];
-  for (let i = 0; i < inv.length; i++){
-    arr.push(inv[i].car_model);
-  };
-
-  return arr.sort();
+  return inv.sort((a,b) => (a.car_model > b.car_model) ? 1 : -1);
 }
-// console.log(sortCarInventory(inventory));
-// console.log("hello");
+
 
 /**
  * ### Challenge `getModelYears`
@@ -255,8 +249,13 @@ function sortCarInventory(inv) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(car_inventory) {
+  let myArr = [];
+  for (let j = 0; j < car_inventory.length; j++){
+    myArr.push(car_inventory[j].car_year);
+  };
+
+  return myArr;
 }
 
 /**
@@ -271,8 +270,17 @@ function getModelYears(/* code here */) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(carData, maxYear) {
+  let arr = [];
+
+  for (let k = 0; k < carData.length; k++) {
+    if (carData[k].car_year <= maxYear ){
+      arr.push(carData[k]);
+    }
+  }
+
+  return arr;
+
 }
 
 /**
@@ -286,8 +294,17 @@ function getOlderCars(/* code here */) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(cars) {
+  let germanArr = [];
+
+  for (let i = 0; i < cars.length; i++){
+    if (cars[i].car_make === "Audi" || cars[i].car_make === "Mercedes-Benz" || cars[i].car_make === "Volkswagen" || cars[i].car_make === "BMW") {
+      germanArr.push(cars[i]);
+    }
+  }
+
+  return germanArr;
+
 }
 
 /**
@@ -308,9 +325,9 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a,b) => a + b; // code here!
+const addFive = (num) => num + 5; // code here!
+const argTimesTwo = (num) => num *2; // code here!
 
 /**
  * ### Challenge `carMaker`
@@ -325,8 +342,15 @@ const argTimesTwo = null; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(num) {
+  const car = {
+    odometer: num,
+    drive: function(distance){
+      this.odometer += distance;
+      return this.odometer;
+    },
+  };
+  return car;
 }
 
 /// ////// END OF CHALLENGE /////////
